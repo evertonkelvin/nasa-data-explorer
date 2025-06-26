bfc625c19eedd33ede72e3c387f2544a7beb2ea6
Everton Kelvin <everton.k23@gmail.com>
2025-06-26 18:08:23 +0100

    Add react routes

---
+++ frontend/src/pages/AsteroidList.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { getAllNasaNeoData } from '../services/api';
+import AsteroidCard from "../components/AsteroidCard";
+
+export default function AsteroidList() {
+  const [nasaNeoData, setNasaNeoData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadNasaNeoData = async () => {
+      try {
+        const neoData = await getAllNasaNeoData();
+        setNasaNeoData(neoData.near_earth_objects);
+      } catch (err) {
+          setError(`Failed to load data: ${err.message}`);
+      } finally {
+          setLoading(false);
+      }
+    }
+
+    loadNasaNeoData();
+  }, []);
+
+  return (
+    <>
+      <h1>NASA Near Earth Objects</h1>
+      <div className="container">
+        {nasaNeoData.map((asteroid) => (
+          <AsteroidCard key={asteroid.id} asteroid={asteroid} />
+        ))}
+      </div>
+    </>
+  );
+}
