25d2cd59819e9e91ec743691cb4b68fcc2f64390
Everton Kelvin <everton.k23@gmail.com>
2025-06-27 11:09:33 +0100

    Add nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard'
-import AsteroidList from './pages/AsteroidList'
+import Dashboard from './pages/Dashboard';
+import AsteroidList from './pages/AsteroidList';
+import NavBar from './components/NavBar';
 
 function App() {
-
+  
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/asteroids" element={<AsteroidList />} />
