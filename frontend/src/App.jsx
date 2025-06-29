67419b20073d95acd5e8d6e012ed2a49dc47eec4
Everton Kelvin <everton.k23@gmail.com>
2025-06-29 03:36:20 +0100

    Add asteroid details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import AsteroidList from './pages/AsteroidList';
+import AsteroidDetail from './pages/AsteroidDetail';
 import NavBar from './components/NavBar';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/asteroids" element={<AsteroidList />} />
+        <Route path="/asteroids/:id" element={<AsteroidDetail />} />
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/components/AsteroidCard.jsx
+++ frontend/src/components/AsteroidCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export default function AsteroidCard({ asteroid }) {
   
   return (
@@ -6,10 +8,12 @@ export default function AsteroidCard({ asteroid }) {
       <p>Diameter: {asteroid.estimated_diameter.kilometers.estimated_diameter_max}</p>
       <p>Orbiting: {asteroid.close_approach_data[0]?.orbiting_body ?? 'No info'}</p>
       <p>Magnitude: {asteroid.absolute_magnitude_h}</p>
-      <p>Hazardous? {asteroid.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}</p>
-      <a href={asteroid.nasa_jpl_url} target="_blank">
-        NASA Link
-      </a>
+      <p>Hazardous: {asteroid.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}</p>
+      <Link
+        to={`/asteroids/${asteroid.id}`}
+        state={{ asteroid }}>
+        See more
+      </Link>
     </div>
   );
 }
